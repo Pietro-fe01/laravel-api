90d9911ca9f68af94983057f0384f2cc7be126cd
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-02-06 15:56:48 +0100

    Anzichè passare l'id tramite la URL, uso la dependences injection passando sempre un ID porject dal frontend in modo che si possa fare il match dei valori dall'oggetto project

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -4,18 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreReviewRequest;
+use App\Models\Project;
 use App\Models\Review;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    public function store($project_id, StoreReviewRequest $request)
+    public function store(Project $project, StoreReviewRequest $request)
     {
         $reviewData = $request->validated();
 
         $new_review = new Review();
-            $new_review->project_id = $project_id;
+            $new_review->project_id = $project->id;
             $new_review->user_name = $reviewData['user_name'];
             $new_review->text_review = $reviewData['text_review'];
             $new_review->review_created = Carbon::now();
